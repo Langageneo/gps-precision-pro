37a8f7d498789618e215bfdb1ec76848764da647
Parfait Guiri <parfait.guiri@gmail.com>
2025-12-01 22:55:57 +0100

    Update gps_module.kt

--- android/gps_module.kt
+++ android/gps_module.kt
@@ -1,28 +1,23 @@
-// android/gps_module.kt
-package com.example.gpsmodule
+package com.gpspro
 
 import android.location.Location
-import android.os.Bundle
 import okhttp3.*
 import org.json.JSONObject
-import java.io.IOException
 
-object GpsUploader {
-    val client = OkHttpClient()
-    val API = "http://YOUR_SERVER:8000/gps-correct"
-
-    fun sendPoint(deviceId: String, loc: Location, timestamp: Long) {
+class GPSModule(private val client: OkHttpClient) {
+    fun sendPosition(loc: Location) {
         val json = JSONObject()
-        json.put("device_id", deviceId)
         json.put("lat", loc.latitude)
         json.put("lon", loc.longitude)
         json.put("accuracy", loc.accuracy)
-        json.put("timestamp", timestamp)
-        val body = RequestBody.create(MediaType.parse("application/json"), json.toString())
-        val req = Request.Builder().url(API).post(body).build()
-        client.newCall(req).enqueue(object : Callback {
-            override fun onFailure(call: Call, e: IOException) { /* log */ }
-            override fun onResponse(call: Call, response: Response) { /* handle */ }
-        })
+        json.put("device_id", "android")
+
+        val body = json.toString().toRequestBody("application/json".toMediaType())
+        val req = Request.Builder()
+            .url("http://10.0.2.2:8000/gps-correct")
+            .post(body)
+            .build()
+
+        client.newCall(req).execute()
     }
 }
